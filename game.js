82e4a7bd842a530a9c853d5e11a44a7ac85acd12
dasar <wanjikuestivah@gmail.com>
2023-07-11 13:18:54 +0300

    single round complete

--- game.js
+++ game.js
@@ -4,11 +4,31 @@ function getComputerChoice() {
     randomNumber=Math.floor(randomNumber);
     if (randomNumber==0){
         return "Rock"
-    } else if(randomNumber==1) {
+    } else if (randomNumber==1) {
         return "Paper"
-    } else{
+    } else {
         return "Scissors";
     }
 }
 
-console.log(getComputerChoice())
+
+function singleRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if (computerSelection == "rock" && playerSelection == "scissors") {
+        return "You lose! Rock beats Scissors";
+    } else if (computerSelection == "rock" && playerSelection == "paper") {
+        return "You win! Paper beats Rock";
+    } else if (computerSelection == "paper" && playerSelection == "scissors") {
+        return "You win! Scissors beats Paper";
+    } else if (computerSelection == "paper" && playerSelection == "rock") {
+        return "You lose! Paper beats Rock";
+    } else if (computerSelection == "scissors" && playerSelection =="paper") {
+        return "You lose! Scissors beats Paper";
+    } else if (computerSelection == "scissors" && playerSelection == "rock") {
+        return "You win! Rock beats Scissors";
+    } else {
+        return "Tie"; 
+    }
+}
+console.log(singleRound("roCK",getComputerChoice()))
